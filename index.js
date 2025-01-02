a27dd957cf24e071dbd2282aee89b5d65a88be88
GodsplanELot <elotgodsplan2@gmail.com>
2025-01-02 03:56:43 +0100

    added an unlink function that can delete files from the directory, its basically a delete button

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const fileOps = async () => {
     try {
         const data = await fsPromises.readFile(path.join(__dirname,'files', 'starter.txt'), 'utf8');
         console.log(data);
+        await fsPromises.unlink(path.join(__dirname,'files', 'starter.txt'));
         await fsPromises.writeFile(path.join(__dirname,'files', 'promiseWrite.txt'), data);
-        await fsPromises.appendFile(path.join(__dirname,'files', 'promiseWrite.txt'), '\n\n Nice to meet you.');
+        await fsPromises.appendFile(path.join(__dirname,'files', 'promiseWrite.txt'), '\n\nNice to meet you.');
         await fsPromises.rename(path.join(__dirname,'files', 'promiseWrite.txt'), path.join(__dirname,'files', 'promiseComplete.txt'));
-        const newData = await fsPromises.readFile(path.join(__dirname,'files', 'starter.txt'), 'utf8');
+        const newData = await fsPromises.readFile(path.join(__dirname,'files', 'promiseComplete.txt'), 'utf8');
         console.log(newData);
     }catch (err) {
         console.error(err);
